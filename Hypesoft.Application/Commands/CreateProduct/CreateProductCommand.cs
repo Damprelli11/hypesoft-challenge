ee3ec243e60fbeed69687a39cc4196dd80f0153d
Damprelli11 <renandamprelli@gmail.com>
2026-01-18 19:26:49 -0300

    chore(application): temporarily remove CQRS artifacts to stabilize build

--- Hypesoft.Application/Commands/CreateProduct/CreateProductCommand.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-using Hypesoft.Application.DTOs;
-
-namespace Hypesoft.Application.Commands.CreateProduct;
-
-public record CreateProductCommand(CreateProductDto Product) : IRequest<Guid>;
