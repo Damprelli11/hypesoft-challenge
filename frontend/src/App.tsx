fb7bd34ced380c5e66f4ef47bf103f2dc83c7148
Damprelli11 <renandamprelli@gmail.com>
2026-01-19 16:27:17 -0300

    chore(frontend): initial Vite + React + Tailwind + shadcn setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Hello World!!! TAILWIND FUNCIONANDO
+      </h1>
+    </div>
+  );
+}
